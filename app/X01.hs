f8dacec6cccfeda18598ec8b423d41bbf9813cbc
Alexey Rykhalskiy <alexey.rykhalskiy@gmail.com>
2023-11-03 23:42:38 +0200

    -- day 1

---
+++ app/X01.hs
@@ -0,0 +1,4 @@
+module X01 where
+
+inc :: (Num a) => a -> a
+inc x = x + 1
---
+++ app/X03.hs
@@ -0,0 +1,25 @@
+module X03 where
+
+-- curried
+add :: Int -> Int -> Int
+add x y = x + y
+
+add2 :: Int -> Int -> Int
+-- infix notation
+add2 x y = x `add` y
+
+sum1 :: Integer
+sum1 = sum [1 .. 10]
+
+-- constant declaration
+val :: Integer
+val = 5
+
+-- Ord - typeclass for comparision
+qs :: (Ord a) => [a] -> [a]
+qs [] = []
+qs [x] = [x]
+qs (x : xs) = qs ys ++ [x] ++ qs zs
+  where
+    ys = [a | a <- xs, a <= x] -- generators
+    zs = [b | b <- xs, b > x]
